c5d155606579c84575ffcab8608903ccece08380
Gloireski <belemgloirebekoutou8@gmail.com>
2024-08-04 00:05:25 +0000

    task 3 fixed

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -54,23 +54,6 @@ def get_db() -> mysql.connector.connection.MYSQLConnection:
     return db_connect
 
 
-"""def get_db() -> mysql.connector.connection.MySQLConnection:
-    define secure connection to db
-    db_host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
-    db_name = os.getenv('PERSONAL_DATA_DB_NAME')
-    db_user = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
-    db_pwd = os.getenv('PERSONAL_DATA_DB_PASSWORD')
-    connection = mysql.connector.connect(
-        host=db_host,
-        port=3306,
-        user=db_user,
-        password=db_pwd,
-        database=db_name,
-    )
-    return connection
-"""
-
-
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
         """
