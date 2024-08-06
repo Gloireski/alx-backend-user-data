a7a445c2c0d458e6d5412459769eac36cb445feb
Gloireski <belemgloirebekoutou8@gmail.com>
2024-08-06 15:04:12 +0000

    task 3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+auth class def
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ auth class """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ returns False - path and excluded_paths
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ returns None - request
+        request is flask object
+        """
+        pass
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ returns None - request
+        request will be the Flask request object
+        """
+        pass
