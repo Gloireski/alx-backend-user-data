baccf28c0ba65a668bde459b7ed1f250f122db9e
Gloireski <belemgloirebekoutou8@gmail.com>
2024-08-07 01:29:21 +0000

    task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 """ Basic authentification """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """ basic auth class """
-    pass
+    def extract_base64_authorization_header(self, auth_header: str) -> str:
+        """the Base64 part of the Authorization header for a
+        Basic Authentication:
+        """
+        if auth_header is None or type(auth_header) is not str:
+            return None
+        if not auth_header.startswith('Basic '):
+            return None
+        return auth_header[6:]
