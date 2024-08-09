6606fa7eed4f35221d4b97e88a17a0b26c09f573
Gloireski <belemgloirebekoutou8@gmail.com>
2024-08-09 00:27:28 +0000

    task 5

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -32,9 +32,13 @@ def before_request():
     request_path_list = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
-        '/api/v1/forbidden/']
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/']
     if auth:
         if auth.require_auth(request.path, request_path_list):
+            if auth.session_cookie(request) is None and auth.session_cookie(
+                    request) is None:
+                abort(401)
             if auth.authorization_header(request) is None:
                 abort(401)
             if auth.current_user(request) is None:
