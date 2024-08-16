4941e2104adfff36fa616e29b341c69c9bcfe93c
Gloireski <belemgloirebekoutou8@gmail.com>
2024-08-16 23:38:27 +0000

    task 4

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python#
+"""define _hash_password
+"""
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """ hash function"""
+    pwd = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(pwd, salt)
