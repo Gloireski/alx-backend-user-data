0d76be4b2c63f7ebd6aab033b3af42cf62842326
Gloireski <belemgloirebekoutou8@gmail.com>
2024-08-17 01:42:41 +0000

    task 11

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """ entry app """
 from auth import Auth
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
+from flask.helpers import make_response
 
 app = Flask(__name__)
 AUTH = Auth()
@@ -22,6 +23,18 @@ def register():
             return jsonify({"email": email, "message": "user created"})
     except Exception:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    response = make_response(jsonify({"email": email,
+                                      "message": "logged in"}))
+    response.set_cookie('session_id', AUTH.create_session(email))
+    return response
 
 
 if __name__ == "__main__":
