fa989659347824e7086fae7643669c7ca47cc75f
Gloireski <35967799+Gloireski@users.noreply.github.com>
2024-08-17 01:02:20 +0100

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,11 +1,26 @@
-"""define _hash_password
+#!/usr/bin/env python3
+"""Auth Class for user attributes validation
 """
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+from user import User
 import bcrypt
+import uuid
 
 
 def _hash_password(password: str) -> str:
     """Takes in password string argument
     Returns bytes (salted_hashed)
     """
-    salt = bcrypt.gensalt()
-    return bcrypt.hashpw(password.encode('utf-8'), salt)
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """Returns string repr of a new UUID
+    Use uuid module
+    """
+    return str(uuid.uuid4())
