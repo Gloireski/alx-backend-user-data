5bd5a0de1e62edd7b1a217f032d00d777f57fb20
Gloireski <belemgloirebekoutou8@gmail.com>
2024-08-17 00:15:03 +0000

    task 5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """Auth Class for user attributes validation
 """
+from sqlalchemy.orm.exc import NoResultFound
+from db import DB
+from user import User
 import bcrypt
 import uuid
 
@@ -17,3 +20,22 @@ def _generate_uuid() -> str:
     Use uuid module
     """
     return str(uuid.uuid4())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ register user
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError("User {} already exists".format(email))
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            new_user = self._db.add_user(email, hashed_password)
+            return new_user
