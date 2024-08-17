d3604a9e7b2b6137009a9c719183fedb0c044644
Gloireski <35967799+Gloireski@users.noreply.github.com>
2024-08-17 01:03:11 +0100

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python3
 """Auth Class for user attributes validation
 """
-
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.exc import NoResultFound
-from sqlalchemy.exc import InvalidRequestError
-from user import User
 import bcrypt
 import uuid
 
